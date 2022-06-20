4d921c1ee5aa4a4c31432e539990796c617215ad
Mouzzam Siddiqui <mohazzamsddq07333@gmail.com>
2022-06-20 10:28:50 +0530

    completed one basic program of recusion

--- JavaLearning/Recursion/BasicRecursionProblem1.java
+++ JavaLearning/Recursion/BasicRecursionProblem1.java
@@ -1,5 +1,14 @@
+// print numbers 1 to 5 using recursion
 public class BasicRecursionProblem1 {
     public static void main(String [] args) {
-        
+        int n = 1;
+        printNumbers(n);
+    }
+     
+    public static void printNumbers(int n) {
+        if(n==6)
+          return;
+        System.out.println(n);
+        printNumbers(n+1);
     }
 }
