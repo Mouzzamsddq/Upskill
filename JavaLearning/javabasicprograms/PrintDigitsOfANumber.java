e7f5f7ce9b776c3909b5ae431756ea6441722593
Mouzzam Siddiqui <mohazzamsddq07333@gmail.com>
2022-06-22 10:31:42 +0530

    print digits of a number added

---
+++ JavaLearning/javabasicprograms/PrintDigitsOfANumber.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class PrintDigitsOfANumber {
+    public static void main(String [] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int nod = countDigits(number);
+        int div = (int)Math.pow(10, nod - 1);
+        while(div != 0) 
+        {
+            int q = number / div;
+            System.out.println(q);
+            number = number % div;
+            div = div / 10;
+        }
+        
+    }
+    public static int countDigits(int n) 
+    {
+        int countDigit = 0;
+        while(n != 0)
+        {
+            countDigit++;
+            n = n/10;
+        }
+        return countDigit;
+    }
+}
