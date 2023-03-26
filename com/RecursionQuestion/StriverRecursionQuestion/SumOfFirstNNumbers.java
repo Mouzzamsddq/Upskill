86dd7224c35b12ef1a6b76bb96f0a32b49a2f131
mouzzamsddq <mouzzam.siddiqui@zopsmart.com>
2023-03-26 17:39:00 +0530

    added summation of n numbers

---
+++ com/RecursionQuestion/StriverRecursionQuestion/SumOfFirstNNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumOfFirstNNumbers {
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(sum(n));
+        sc.close();
+        
+    }
+    
+    public static int sum(int n) {
+        if(n == 0) {
+            return 0;
+        }
+        
+        return n + sum(n - 1);
+    }
+}
