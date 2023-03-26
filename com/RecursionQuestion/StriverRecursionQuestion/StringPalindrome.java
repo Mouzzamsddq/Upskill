7b42194cb606e304d5591116c21aea663ff39f40
mouzzamsddq <mouzzam.siddiqui@zopsmart.com>
2023-03-26 12:21:03 +0530

    updated string palindrome code

--- com/RecursionQuestion/StriverRecursionQuestion/StringPalindrome.java
+++ com/RecursionQuestion/StriverRecursionQuestion/StringPalindrome.java
@@ -4,17 +4,18 @@ public class StringPalindrome {
     public static void main(String [] args) {
         Scanner sc = new Scanner(System.in);
         String  s = sc.next();
-        System.out.println(isPalindrome(0, s.length() - 1, s));
-        
+        System.out.println(isPalindrome(0, s));
+        sc.close();
     }
 
-    public static boolean isPalindrome(int left, int right, String s) {
+    public static boolean isPalindrome(int left, String s) {
+        int right = s.length() - left - 1;
         if(left >= right) {
             return true;
         }
         boolean isEqual = s.charAt(left) == s.charAt(right);
         if(isEqual) {
-            return isPalindrome(++left, --right, s);
+            return isPalindrome(++left, s);
         }
         return isEqual;
     }
