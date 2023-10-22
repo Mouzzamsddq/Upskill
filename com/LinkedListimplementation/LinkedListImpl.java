a2d7623ae91807d465d51a5c46c33e686403e5c1
MouzzamSiddiqui07 <mouzzam.siddiqui@zopsmart.com>
2023-10-22 09:36:43 +0530

    reverse linkedlist di and pi both

--- com/LinkedListimplementation/LinkedListImpl.java
+++ com/LinkedListimplementation/LinkedListImpl.java
@@ -103,10 +103,78 @@ public class LinkedListImpl {
                 return tempHead.data;
             }
         }
+
+        public void addAt(int index, int data) {
+            if(index < 0 || index >= size) {
+                System.out.println("Invalid args");
+            } else if(index == 0) {
+                addFirst(data);
+            } else if(index == size - 1) {
+                addLast(data);
+            } else {
+                Node indexedNode = new Node(data);
+                Node tempHead = head;
+                for(int i = 0 ; i < index - 1 ; i++) {
+                    tempHead = tempHead.next;
+                }
+                indexedNode.next = tempHead.next;
+                tempHead.next = indexedNode;
+                size++;
+            }
+        }
+
+        public void removeLast() {
+            if(head == null) {
+                System.out.println("Linkedlist is empty..!");
+            } else if(size == 1) { 
+                head = null;
+                size--;
+            } else {
+                Node tempHead = head;
+                while(tempHead.next.next != null) {
+                    tempHead = tempHead.next;
+                }
+                tempHead.next = null;
+                size--;
+            }
+        }
+
+        public Node getNodeAt(int index) {
+             Node tempHead = head;
+             for(int i = 0 ; i < index ; i++) {
+                 tempHead = tempHead.next;
+             }
+             return tempHead;
+        }
+        
+        public void reverseDI() {
+            int li = 0;
+            int ri = size - 1;
+            while(li <= ri) {
+                Node leftNode = getNodeAt(li);
+                Node rightNode = getNodeAt(ri);
+                int temp = leftNode.data;
+                leftNode.data = rightNode.data;
+                rightNode.data = temp;
+
+                li++;
+                ri--;
+            }
+        }
+
+        public void reversePI() {
+            Node current = head;
+            Node prev = null;
+            while(current != null) {
+                Node temp = current.next;
+                current.next = prev;
+                prev = current;
+                current = temp;
+            }
+            head = prev;
+        }
      
     }
-
-
 
     public static void main(String []args) {
        LinkedList linkedList = new LinkedList();
@@ -125,7 +193,32 @@ public class LinkedListImpl {
        linkedList.addLast(30);
        linkedList.addLast(40);
        linkedList.print();
-       System.out.println(linkedList.getAt(3));
-    
+       System.out.println(linkedList.getAt(2));
+       linkedList.print();
+       linkedList.addAt(3, 25);
+       linkedList.print();
+       linkedList.removeLast();
+       linkedList.print();
+       linkedList.removeLast();
+       linkedList.print();
+       linkedList.removeFirst();
+       linkedList.removeFirst();
+       linkedList.removeFirst();
+       linkedList.print();
+       linkedList.removeLast();
+       linkedList.print();
+       linkedList.addLast(10);
+       linkedList.addLast(20);
+       linkedList.addLast(30);
+       linkedList.addLast(40);
+       linkedList.addLast(50);
+       linkedList.addLast(60);
+       linkedList.print();
+       System.out.println("After perform reverse DI");
+       linkedList.reverseDI();
+       linkedList.print();
+       System.out.println("After perform reverse PI");
+       linkedList.reversePI();
+       linkedList.print();
     }
 }
