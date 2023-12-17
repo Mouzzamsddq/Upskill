cdf04baa0e2e40b23ff419ca27814a55fedbd130
MouzzamSiddiqui07 <mouzzam.siddiqui@zopsmart.com>
2023-12-17 11:45:28 +0530

    completed converting array to ll

--- DSA_prep_17_Sep_to_31_mar/LinkedListStriver/LinkedListImpl.java
+++ DSA_prep_17_Sep_to_31_mar/LinkedListStriver/LinkedListImpl.java
@@ -2,6 +2,27 @@ public class LinkedListImpl {
     public static void main(String []args) {
         Node node = new Node(10);
         System.out.println(node.data);
+        
+
+        // creating array
+        int[] arr = new int[]{1,5,13,12,16};
+        Node head = convertArrToLL(arr);
+        System.out.println(head.next.data);
+
+    }
+
+    public static Node convertArrToLL(int[] arr) {
+        if(arr.length == 0) {
+            return null;
+        }
+        Node head = new Node(arr[0]);
+        Node temp = head;
+        for(int i = 1 ; i < arr.length ; i++) {
+            Node latestNode = new Node(arr[i]);
+            temp.next = latestNode;
+            temp = latestNode;
+        }
+        return head;
     }
 }
 
