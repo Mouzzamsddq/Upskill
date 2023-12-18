d5a9fef5f64847966e423bef25e1d9a7f04fac85
MouzzamSiddiqui07 <mouzzam.siddiqui@zopsmart.com>
2023-12-19 00:00:45 +0530

    completed doubly linkedlist basic question

--- DSA_prep_17_Sep_to_31_mar/LinkedListStriver/DoublyLinkedListImpl.java
+++ DSA_prep_17_Sep_to_31_mar/LinkedListStriver/DoublyLinkedListImpl.java
@@ -3,7 +3,7 @@ public class DoublyLinkedListImpl {
           //create an array with 5 values
           int []arr = new int[]{10,20,30,40,50};
           Node headNode = convertArrToDll(arr);
-          headNode = deleteFromBeg(headNode);
+          headNode = insertAtGivenNode(headNode, 25, 60);   
           printDLL(headNode);
     }
 
@@ -78,6 +78,98 @@ public class DoublyLinkedListImpl {
     head = newNode;
     return head;
 }
+
+/*
+ * T.C. - O(n)
+ * S.C. - O(1)
+ */
+public static Node insertBeforeTail(Node head, int K) {
+         Node newNode = new Node(K);
+         if(head == null) {
+             head = newNode;
+             return head;
+         }
+         if(head.next == null) {
+             newNode.next = head;
+             head.prev = newNode;
+             head = newNode;
+             return head;
+         }
+         Node temp = head;
+         while(temp.next != null && temp.next.next != null) {
+             temp = temp.next;
+         }
+         newNode.next = temp.next;
+         newNode.prev = temp;
+         temp.next.prev = newNode;
+         temp.next = newNode;
+         return head;
+      }
+
+      /*
+       * T.C. - O(k)
+       * S.C - O(1)
+       */
+      public static Node insertBeforeKthNode(Node head, int k, int val) {
+        if(k < 1) {
+            return head;
+        }
+        if(k == 1) {
+            Node n = new Node(val);
+            n.next = head;
+            if(head != null) {
+                head.prev = n;
+            }
+            head = n;
+            return head;
+        }
+
+        int count = 1;
+        Node temp = head;
+        while(temp != null) {
+            if(count == k - 1) {
+                Node n = new Node(val);
+                n.next = temp.next;
+                n.prev = temp;
+                temp.next.prev = n;
+                temp.next = n;
+                break;
+            }
+            temp = temp.next;
+            count++;
+        }
+        return head;
+    }
+
+    public static Node insertAtGivenNode(Node head, int val, int x) {
+        // Write Your Code Here.
+        if(head == null) {
+            Node n = new Node(val);
+            head = n;
+            return head;
+        }
+        if(head.data == x) {
+            Node n = new Node(val);
+            n.next = head;
+            head.prev = n;
+            head = n;
+            return head;
+        }
+
+        Node temp = head;
+        while(temp.next != null) {
+            if(temp.next.data == x) {
+                Node n = new Node(val);
+                n.next = temp.next;
+                n.prev = temp;
+                temp.next.prev = n;
+                temp.next = n;
+                break;
+            }
+            temp = temp.next;
+        }
+        return head;
+    }
 }
 
 
